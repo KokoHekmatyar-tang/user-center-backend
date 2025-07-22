379f97fb7253f53b40097b53980e74f02918fc95
tang <ynkk91@163.com>
2025-07-22 16:53:38 +0800

    user-center

---
+++ src/main/java/com/tang/usercenter/UserCenterApplication.java
@@ -0,0 +1,20 @@
+package com.tang.usercenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ */
+@SpringBootApplication
+@MapperScan("com.tang.usercenter.mapper")
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/tang/usercenter/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.tang.usercenter.mapper;
+
+import com.tang.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
